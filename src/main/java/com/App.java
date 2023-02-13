a960159d16362dea1703e7955d4653132dc838c3
shi-vam7902 <shivashah.glsbca20@gmail.com>
2023-02-13 08:55:58 +0530

    Crud Done..

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -7,6 +7,7 @@ import javax.management.Query;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.entity.UserEntity;
@@ -21,23 +22,24 @@ public class App {
 
 		System.out.println("Hello World!");
 		int choice;
-		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-		SessionFactory sf = cfg.buildSessionFactory();
-		//Session created using sessionFactory -> sessionFactory will read all confgiuration -> 
-		//session -> query execute .. 
-		
-		//insert list delete 
-		
+		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");// hibernate -> data-> execute
+		SessionFactory sf = cfg.buildSessionFactory();// build
+		// Session created using sessionFactory -> sessionFactory will read all
+		// confgiuration ->
+		// session -> query execute ..
+
+		// insert list delete
 
 		Session session = sf.openSession();
 		UserEntity user = new UserEntity();
-		System.out.println(session);
 
+		System.out.println(session);
 		while (true) {
 			System.out.println("0. for Exit");
 			System.out.println("1. for Insert");
-			System.out.println("2. for Update");
+			System.out.println("2. for Get");
 			System.out.println("3. for Delete");
+			System.out.println("4. for update");
 			System.out.println("Enter Your Choice : => ");
 			choice = scr.nextInt();
 
@@ -70,16 +72,41 @@ public class App {
 				break;
 
 			case 3:
-				
-				//delete from users where userId = 9 
+
+				// delete from users where userId = 9
 				System.out.println("Enter userId...");
-				int userId= scr.nextInt();
-				
-				UserEntity user1 =  session.get(UserEntity.class, userId); //select * from users where userid = 1 ;
-				session.delete(user1);
-				
-				//commit 
-				
+				int userId = scr.nextInt();
+
+				Transaction tx = session.beginTransaction();
+
+				UserEntity user1 = session.get(UserEntity.class, userId); // select * from users where userid = 1 ;
+				try {
+					session.delete(user1);
+				} catch (Exception e) {
+					tx.rollback();
+				}
+				// commit
+				tx.commit();
+				break;
+			case 4:
+				System.out.println("Enter userId... For Update");
+				int userkey = scr.nextInt();
+				UserEntity useR = session.get(UserEntity.class, userkey); // select * from users where userid = 1 ;
+				if (useR == null) {
+					System.out.println(" User id is Wrong");
+				} else {
+					Transaction tx1 = session.beginTransaction();
+					try {
+						// tdl is for transcation we need to commit
+
+						System.out.println("Your UserName is +" + useR.getUserName());
+						System.out.println("Enter New UserName or Enter Old One ");
+						useR.setUserName(scr.next());
+						session.update(useR);
+					} catch (Exception e) {
+						tx1.commit();
+					}
+				}
 				break;
 
 			default:
@@ -89,5 +116,5 @@ public class App {
 		}
 
 	}
-	
+
 }
---
+++ src/main/java/com/entity/ProductEntity.java
@@ -0,0 +1,23 @@
+package com.entity;
+
+public class ProductEntity {
+	
+	private Integer prodId;
+	private String prodName;
+
+	public Integer getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(Integer prodId) {
+		this.prodId = prodId;
+	}
+
+	public String getProdName() {
+		return prodName;
+	}
+
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+}
